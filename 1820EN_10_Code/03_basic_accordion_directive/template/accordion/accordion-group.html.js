3f724ce26e472fa3307a814461f58a1cf00199e0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-06-01 16:27:33 +0200

    chore(): add generated template modules to gitignore

--- 1820EN_10_Code/03_basic_accordion_directive/template/accordion/accordion-group.html.js
+++
@@ -1,9 +0,0 @@
-angular.module("accordion-group.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("accordion-group.html",
-    "<div class=\"accordion-group\">" +
-    "  <div class=\"accordion-heading\" ><a class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\">{{heading}}</a></div>" +
-    "  <div class=\"accordion-body\" ng-show=\"isOpen\">" +
-    "    <div class=\"accordion-inner\" ng-transclude></div>" +
-    "  </div>" +
-    "</div>");
-}]);
