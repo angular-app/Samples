7deec102fc6df5210eeb9f45689cb33e4112a7e5
Pete Bacon Darwin <pete@bacondarwin.com>
2013-11-27 09:35:56 +0000

    chore(karma): update to 0.10.x

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,76 @@
+// Karma configuration
+// Generated on Wed Nov 27 2013 09:32:17 GMT+0000 (GMT Standard Time)
 
-// base path, that will be used to resolve files and exclude
-basePath = '.';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'lib/jquery-1.8.3/jquery-1.8.3.js',
-  'lib/jquery-ui-1.9.2/js/datepicker.js',
-  'lib/angular/1.0.4/angular.js',
-  'lib/angular/1.0.4/angular-mocks.js',
-  '1820EN_*/**/*.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [
-  '1820EN_01_Code/**',
-  '1820EN_02_Code/10 - testing - karma tips and tricks/**',
-  '1820EN_05_Code/**',
-  '1820EN_06_Code/**',
-  '1820EN_09_Code/01_directive_test_skeleton/test.js'
-];
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = ['Chrome'];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// test results reporter to use
-// possible values: dots || progress
-reporter = 'progress';
 
-// web server port
-port = 9018;
+    // list of files / patterns to load in the browser
+    files: [
+      'lib/jquery-1.8.3/jquery-1.8.3.js',
+      'lib/jquery-ui-1.9.2/js/datepicker.js',
+      'lib/angular/1.0.4/angular.js',
+      'lib/angular/1.0.4/angular-mocks.js',
+      '1820EN_*/**/*.js'
+    ],
 
-// cli runner port
-runnerPort = 9100;
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // list of files to exclude
+    exclude: [
+      '1820EN_01_Code/**',
+      '1820EN_02_Code/10*/**',
+      '1820EN_05_Code/**',
+      '1820EN_06_Code/**',
+      '1820EN_09_Code/01*/**'
+    ],
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = true;
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
